e5fdfe0d8996b6d93d9e07e23b6964a6c3729cdc
Johannes <bunk@uni-bremen.de>
2017-04-23 01:53:07 +0200

    hab beim testen ein paar fehler gefunden

--- Abgabe 1/Aufgabe 3/engine/engine.hpp
+++ Abgabe 1/Aufgabe 3/engine/engine.hpp
@@ -1,13 +1,11 @@
 struct Trajectory{
-        int values[10][2]={0};
+	int values[10][2]={0};
 };
 
 struct Pattern{
 	Trajectory servos[8];
 };
 
-int add_pattern(int pattern_id);
-int add_trajectory(int pattern_id, int servo_id);
 int add_tuple(int pattern_id, int servo_id, int time, int angle);
 int get_angle(int pattern_id, int servo_id, int time);
 int get_estimate(Trajectory t, int time);
