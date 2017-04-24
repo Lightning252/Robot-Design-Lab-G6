641686d2cd6d13d9a78a249cecb0717348019a1e
Unknown <bunk@uni-bremen.de>
2017-04-24 19:14:04 +0200

    tests

--- Abgabe 1/Aufgabe 3/engine/main.cpp
+++ Abgabe 1/Aufgabe 3/engine/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "engine.hpp"
 
+//tests
 int main()
 {
 	std::cout << (add_tuple(0,0,0,10)==0) << std::endl;
@@ -22,3 +23,33 @@ int main()
 	std::cout << (get_angle(0,0,180)==28) << std::endl;
 	return 0;
 }
+
+/*
+output:
+
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+
+valgrind:
+==2711== 
+==2711== HEAP SUMMARY:
+==2711==     in use at exit: 0 bytes in 0 blocks
+==2711==   total heap usage: 2 allocs, 2 frees, 73,728 bytes allocated
+==2711== 
+==2711== All heap blocks were freed -- no leaks are possible
+==2711== 
+==2711== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+==2711== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+*/
+
