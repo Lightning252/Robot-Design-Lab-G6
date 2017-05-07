73a003bbbf058aead7071b50c3a4ea6d0dcf5ce2
Johannes Bunk <bunk@uni-bremen.de>
2017-05-08 00:24:57 +0200

    1 weiter

--- Aufgabe 2/firmware/servo.cpp
+++ Aufgabe 2/firmware/servo.cpp
@@ -1,6 +1,7 @@
 #include "servo.hpp"
 #include <stdint.h>
 #include "inc/stm32f10x_tim.h"
+#include "inc/stm32f10x_gpio.h"
 
 /**
 I/O_VCC1 FR_1 front right hip PA6
@@ -19,13 +20,8 @@ void servo_setAngle(enum Servos servo, int value){
 if(value < 0 || value > 1700){
 return;
 }
-
-//do the math
-float impTime = 1.6 / 170;
-impTime = (impTime * value) + 0.6;
-
-
-int signal = 0;
+int angle = value / 10;
+int signal = (115200 / 170) * angle + 43200;
 
 switch(servo){
 case 0:
@@ -64,27 +60,26 @@ break;
 
 void servo_init(){
 TIM_TimeBaseInitTypeDef timBase;
-
 TIM_TimeBaseStructInit (&timBase);
-timBase.TIM_Prescaler = ((SystemCoreClock / 2 ) / 1000000)-1; //?
-timBase.TIM_ClockDivision = TIM_CKD_DIV1;
-timBase.TIM_CounterMode = TIM_CounterMode_Up;
-timBase.TIM_Period = 2000;
+timBase.TIM_Prescaler = 22
+timBase.TIM_ClockDivision = TIM_CKD_DIV1; //?
+timBase.TIM_CounterMode = TIM_CounterMode_Up; //?
+timBase.TIM_Period = 65455;
 TIM_TimeBaseInit (TIM3, &timBase);
-TIM_ARRPreloadConfig (TIM3, ENABLE);
+//TIM_ARRPreloadConfig (TIM3, ENABLE); //?
 TIM_TimeBaseInit (TIM4, &timBase);
-TIM_ARRPreloadConfig (TIM4, ENABLE);
+//TIM_ARRPreloadConfig (TIM4, ENABLE); //?
 
 // Output compare match
 TIM_OCInitTypeDef oc;
 TIM_OCStructInit (&oc);
 
-oc.TIM_OCIdleState = TIM_OCIdleState_Reset;
-oc.TIM_OCNIdleState = TIM_OCNIdleState_Set;
-oc.TIM_OCMode = TIM_OCMode_PWM1;
-oc.TIM_OCPolarity = TIM_OCPolarity_High;
-oc.TIM_OutputState = TIM_OutputState_Enable;
-oc.TIM_Pulse = 1; // Initiale Pulsweite in Millisekunden
+//oc.TIM_OCIdleState = TIM_OCIdleState_Reset;
+//oc.TIM_OCNIdleState = TIM_OCNIdleState_Set;
+oc.TIM_OCMode = TIM_OCMode_PWM1; //?
+oc.TIM_OCPolarity = TIM_OCPolarity_High; //?
+oc.TIM_OutputState = TIM_OutputState_Enable; //?
+oc.TIM_Pulse = 0; 
 
 TIM_OC1Init (TIM3, &oc);
 TIM_OC1PreloadConfig (TIM4, TIM_OCPreload_Enable);
@@ -110,18 +105,14 @@ TIM_OC3PreloadConfig (TIM4, TIM_OCPreload_Enable);
 TIM_OC4Init (TIM4, &oc);
 TIM_OC4PreloadConfig (TIM4, TIM_OCPreload_Enable);
 
-/**
-TIM_BDTRInitTypeDef bdtr;
-TIM_BDTRStructInit (&bdtr);
-bdtr.TIM_AutomaticOutput = TIM_AutomaticOutput_Enable;
-TIM_BDTRConfig (TIM4, &bdtr);*/
 
 //connect channels to GPIO -> PA6, PA7, PB0, PB1, PB6, PB7, PB8, PB9 ?
-
-/**
-void TIM_DMACmd(TIM_TypeDef* TIMx, u16 TIM_DMASource, FunctionalState NewState);
-void TIM_CtrlPWMOutputs(TIM_TypeDef* TIMx, FunctionalState NewState);
-*/
+GPIO_InitTypeDef gpioStructure;
+GPIO_StructInit(&gpioStructure);
+gpioStructure.GPIO_Pin = GPIO_Pin_22;
+//gpioStructure.GPIO_Mode = GPIO_Mode_AF_PP; //?
+gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_Init(GPIOA,&gpioStructure); 
 
 // Start the timer
 TIM_Cmd (TIM3, ENABLE);
